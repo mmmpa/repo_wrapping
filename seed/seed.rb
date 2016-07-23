80bae5c51716c413dce7e2160833c62aaab766ed
mmmpa <mmmpa.mmmpa@gmail.com>
2016-07-24 07:02:24 +0900

    updated

--- seed/seed.rb
+++ seed/seed.rb
@@ -1,10 +1,11 @@
 #!/usr/bin/env ruby
 require File.expand_path('../../wrapped/config/application', __FILE__)
+abort("The Rails environment is running in production mode!") if Rails.env.production?
 Rails.application.require_environment!
 
 require 'seed-fu'
 
-100.times do |n|
+10000.times do |n|
   ModelA.seed(
     id: n,
     name: SecureRandom.hex(4)
